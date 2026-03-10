bb3a80832f52c5a9480f144b0cedc8fb2c4b7cda
meggggann <2504009@sit.singaporetech.edu.sg>
2026-03-10 18:15:53 +0800

    changed title

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Luxe Secondhand</a>
+        <a class="navbar-brand" href="index.php">Luxury Secondhand</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
