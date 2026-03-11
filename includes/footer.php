56ccf0e8dfdf246e5c47132dd72c158a4c76bc2a
xavier583 <xaviersohyanheng@gmail.com>
2026-03-11 23:16:45 +0800

    Minor changes here and there

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,17 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="js/main.js"></script>
-</footer class ="bg-light text-center text-lg-start">
+<footer class ="bg-light text-center text-lg-start">
 
-    <!Tagline>
-    <div class ="text-center p-2">
-        <h2>Luxury evolves. Style Endures.</h2>
-        <h2>Every piece deserves another story.</h2>
+    <!-- Tagline -->
+    <div class ="text-center py-3">
+        <p class ="fs-4 fw-bold" style = "letter-spacing: 1.5px;">
+            Luxry evolves. Style Endures.
+        </p>
+        <p class = "fs-5">
+            Every Piece deserves another story.
+        </p>
     </div>
-    <!Copyright>
+    <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         &copy; 2024 Maison Reluxe. All rights reserved.
     </div>
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php include 'includes/header.php'; ?>
 <?php include 'includes/nav.php'; ?>
 
-<main class="container">
-    <h1>Welcome to Maison Reluxe</h1>
-    <p>Your one-stop shop for luxury secondhand goods.</p>
+<main class="container mt-5 mb-5">
+    <h1 class = "display-4">Maison Reluxe</h1>
+    <p class = "lead">
+        Curated Luxury. Timeless Style. A second life for iconic pieces</p>
 </main>
 
 <?php include 'includes/footer.php'; ?>
