0a3d898f7226da1ac7835eddaee97cb5940240ec
xavier583 <xaviersohyanheng@gmail.com>
2026-03-17 14:49:30 +0800

    Basic standard products being displayed

--- Products/jewellery.php
+++ Products/jewellery.php
@@ -1,5 +1,5 @@
-<?php include 'includes/header.php'; ?>
-<?php include 'includes/nav.php'; ?>
+<?php include '../includes/header.php'; ?>
+<?php include '../includes/nav.php'; ?>
 
 <div class = "container mt-5">
     <h1>Jewellery Collection</h1>
@@ -8,7 +8,7 @@
         <!--- Product 1 -->
         <div class = "col-md-4 mb-4">
             <div class = "card h-100 text-center">
-                <img src = "images\cartier_ring.jpg" class = "card-img-top" alt = "Cartier Ring">
+                <img src = "../images/cartier_ring.jpg" class = "card-img-top" alt = "Cartier Ring">
                 <div class = "card-body">
                     <h5 class = "card-title">Cartier Ring</h5>
                     <p class = "card-text">$4500</p>
@@ -20,7 +20,7 @@
         <!--- Product 2 -->
         <div class = "col-md-4 mb-4">
             <div class = "card h-100 text-center">
-                <img src = "images/bvlgari_necklace.jpg" class = "card-img-top" alt = "Bvlgari Necklace">
+                <img src = "../images/bvlgari_necklace.jpg" class = "card-img-top" alt = "Bvlgari Necklace">
                 <div class = "card-body">
                     <h5 class = "card-title">Bvlgari Necklaces</h5>
                     <p class = "card-text">$6400</p>
@@ -32,7 +32,7 @@
         <!--- Product 3 -->
         <div class = "col-md-4 mb-4">
             <div class = "card h-100 text-center">
-                <img src = "images/tiffany_necklace.jpg" class = "card-img-top" alt = "Tiffany Necklace">
+                <img src = "../images/tiffany_necklace.jpg" class = "card-img-top" alt = "Tiffany Necklace">
                 <div class = "card-body">
                     <h5 class = "card-title">Tiffany Necklace</h5>
                     <p class = "card-text">$5200</p>
@@ -45,4 +45,4 @@
     </div>
 </div>
 
-<?php include 'includes/footer.php'; ?>
+<?php include '../includes/footer.php'; ?>
