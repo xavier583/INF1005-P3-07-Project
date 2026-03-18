1a4bb9f234531a34243e44f66701eb90d1256a62
meggggann <2504009@sit.singaporetech.edu.sg>
2026-03-19 00:39:44 +0800

    updated/cleaned up jewellery page

--- register.php
+++ register.php
@@ -3,19 +3,22 @@
 
 <form action="process_register.php" method="post" class="form-box">
     <h1>Member Registration</h1>
-     <p>
+    <p>
         Have an account? <a href="login.php">Sign in</a>
     </p>
     <div class="form-group">
-        <label for="fname">First Name:</label>
-        <input type="text" id="fname" name="fname" placeholder="Enter first name">
+        <label for="fname">Username:</label>
+        <input type="username" id="username" name="username" placeholder="Enter username" pattern="[A-Za-z0-9_]{4,20}" required>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $username = $_POST["fname"];
+            if (!preg_match("/^[a-zA-Z0-9_]{4,20}$/", $username)) {
+                echo "<p style='color: red;'>Username must be between 4 and 20 characters and can only contain letters, numbers, and underscores.</p>";
+            }
+            $username = htmlspecialchars(trim($_POST['username']));
+        }
+        ?>
     </div>
-
-    <div class="form-group">
-        <label for="lname">Last Name:</label>
-        <input type="text" id="lname" name="lname" placeholder="Enter last name">
-    </div>
-
     <div class="form-group">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" placeholder="Enter email">
@@ -37,7 +40,7 @@
     </div>
 
     <button type="submit">Register</button>
-    
+
 </form>
 
 <?php include 'includes/footer.php'; ?>
